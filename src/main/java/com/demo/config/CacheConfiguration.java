31121c33c4024e3f83da4c200a45c2ed455ede32
devalgas <kamgadevalgas@gmail.com>
2024-09-01 01:48:38 +0200

    second commit

--- src/main/java/com/demo/config/CacheConfiguration.java
+++ src/main/java/com/demo/config/CacheConfiguration.java
@@ -48,6 +48,18 @@ public class CacheConfiguration {
             createCache(cm, com.demo.domain.User.class.getName());
             createCache(cm, com.demo.domain.Authority.class.getName());
             createCache(cm, com.demo.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.demo.domain.Region.class.getName());
+            createCache(cm, com.demo.domain.Country.class.getName());
+            createCache(cm, com.demo.domain.Location.class.getName());
+            createCache(cm, com.demo.domain.Department.class.getName());
+            createCache(cm, com.demo.domain.Department.class.getName() + ".employees");
+            createCache(cm, com.demo.domain.Task.class.getName());
+            createCache(cm, com.demo.domain.Task.class.getName() + ".jobs");
+            createCache(cm, com.demo.domain.Employee.class.getName());
+            createCache(cm, com.demo.domain.Employee.class.getName() + ".jobs");
+            createCache(cm, com.demo.domain.Job.class.getName());
+            createCache(cm, com.demo.domain.Job.class.getName() + ".tasks");
+            createCache(cm, com.demo.domain.JobHistory.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
